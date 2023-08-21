08e46608aaa5dd94150e30f271ce1a95dc2c828d
Jdillon297 <JACOBDILLON119@GMAIL.COM>
2023-08-21 13:06:09 -0500

    Added User enitity and userDto

---
+++ SECURED-WEB/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿namespace SECURED_WEB.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string PhoneNumber { get; set; } = string.Empty;
+}
+
+
+
+public class UserDto
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string PhoneNumber { get; set; } = string.Empty;
+}
+   
