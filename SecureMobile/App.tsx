bcd3ab9887ac6965ba9d7c08da322c38ebd7f38e
Marquinoli <mfigueroaferez@gmail.com>
2023-09-05 14:54:04 -0500

    running react-native first time files

--- SecureMobile/App.tsx
+++ SecureMobile/App.tsx
@@ -80,7 +80,7 @@ function App(): JSX.Element {
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
-          <Section title="See Your Changes">
+          <Section title="I'M SO EXCITED FOR THIS PROJECT">
             <ReloadInstructions />
           </Section>
           <Section title="Debug">
