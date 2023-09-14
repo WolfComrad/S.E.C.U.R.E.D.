83ba32afbca108e3e032b2b0f24d90ed69d1f523
Jdillon297 <JACOBDILLON119@GMAIL.COM>
2023-09-14 10:39:29 -0500

    Added some Icons wip

--- SecureMobile/screens/HomeScreen.tsx
+++ SecureMobile/screens/HomeScreen.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import {Text, View, Pressable} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import LogoutButton from '../android/components/LogoutButton';
 
 const HomeScreen = () => {
   const navigation = useNavigation<NativeStackNavigationProp<any>>();
   return (
     <View>
-      <Text>This is our Home screen!</Text>
       <View>
+        <LogoutButton />
         <Pressable
           onPress={() => {
             navigation.navigate('Login');
