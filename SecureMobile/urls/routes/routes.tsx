e51147b6a04319681834cd294fbe064079f1046e
Jdillon297 <JACOBDILLON119@GMAIL.COM>
2023-09-19 19:59:16 -0500

    added the whoami rout

--- SecureMobile/urls/routes/routes.tsx
+++ SecureMobile/urls/routes/routes.tsx
@@ -3,4 +3,5 @@ export const apiRoutes = {
   login: `${JACOBS_IP}/api/Authorization/login/`,
   register: `${JACOBS_IP}/api/Authorization/register`,
   logout: `${JACOBS_IP}/api/Authorization/logout`,
+  whoami: `${JACOBS_IP}/api/Authorization/whoami`,
 } as const;
