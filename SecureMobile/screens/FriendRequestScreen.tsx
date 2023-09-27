52b38adb08de62f88285071b5de434ff0634f4d9
Marquinoli <mfigueroaferez@gmail.com>
2023-09-27 12:02:20 -0500

    removed error alert & fixed styles routes

--- SecureMobile/screens/FriendRequestScreen.tsx
+++ SecureMobile/screens/FriendRequestScreen.tsx
@@ -1,6 +1,6 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-import { styles } from '../styles';
+import { styles } from '../styles/styles';
 
 const FriendRequestScreen = () => {
   return (
